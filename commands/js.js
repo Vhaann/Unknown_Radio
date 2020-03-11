02d01792ff1e07b911988936678a2d290810032a
Koko <koriantin@hotmail.fr>
2020-03-11 09:15:49 +0100

    ajout id ghom

--- commands/js.js
+++ commands/js.js
@@ -1,33 +1,34 @@
+const {
+    RichEmbed
+} = require('discord.js')
 
-const { RichEmbed } = require('discord.js')
+module.exports = async function exe(message, js) {
 
-module.exports = async function exe ( message, js ) {
+    if (!['126292280700043264', '352176756922253321'].includes(message.author.id)) return
 
-    if(message.author.id !== '126292280700043264') return
-
-    js = js.replace('```js','').replace('```','').trim()
+    js = js.replace('```js', '').replace('```', '').trim()
 
     const embed = new RichEmbed()
     let result = 'té nul'
-    try{
-       result = await eval('async () => {' + js + '}')()
-    }catch (error) {
+    try {
+        result = await eval('async () => {' + js + '}')()
+    } catch (error) {
         embed.setDescription(format(error.message))
         return message.channel.send(embed).catch()
     }
-    try{
+    try {
         embed.setTitle(typeof result)
         embed.setDescription(format(result))
-    }catch(error){
-        try{
+    } catch (error) {
+        try {
             embed.setDescription(format(JSON.stringify(result, null, 2)))
-        }catch(error2){
+        } catch (error2) {
             embed.setDescription(format(error.message))
         }
     }
     message.channel.send(embed).catch()
 }
 
-function format ( text ){
+function format(text) {
     return '```js\n' + String(text) + '\n```'
 }
--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const config = require("./config")
-const { readdir } = require("fs").promises
-const { Client, Collection } = require("discord.js")
-const { resolve } = require('path')
+const {
+    Client,
+    Collection
+} = require("discord.js")
 
 const client = new Client({
     fetchAllMembers: true
 })
+const config = require("./config")
+const {
+    readdir
+} = require("fs").promises
+const {
+    resolve
+} = require('path')
 
-client.login( config.token ).catch()
+client.login(config.token).catch()
 
-client.once('ready',async () => {
+client.once('ready', async () => {
     const commandDir = await readdir('./commands')
     client.commands = new Collection()
-    commandDir.forEach( commandFileName => {
+    commandDir.forEach(commandFileName => {
         client.commands.set(
             commandFileName.replace(/\.js$/, ''),
             require(resolve(__dirname, 'commands', commandFileName))
         )
-    } )
+    })
     client.ready = true
-    console.log('Client Ready ! Commands: ', client.commands.map((c,n)=>n).join(', '))
-} )
+    console.log('Client Ready ! Commands: ', client.commands.map((c, n) => n).join(', '))
+})
 client.on("message", async (message) => callCommand(message))
-client.on('messageUpdate',async (oldMessage, newMessage) => callCommand(newMessage))
+client.on('messageUpdate', async (oldMessage, newMessage) => callCommand(newMessage))
 
-function callCommand ( message ){
-    if( !client.ready || !message.author || message.author.bot || message.system ) return
-    if( message.content.startsWith(config.prefix) ) {
+function callCommand(message) {
+    if (!client.ready || !message.author || message.author.bot || message.system) return
+    if (message.content.startsWith(config.prefix)) {
         const commandName = message.content.slice(config.prefix.length).split(/\s+/)[0]
         const args = message.content.replace(config.prefix + commandName, '').trim()
         const command = client.commands.get(commandName)
-        if(command) command( message, args || '')
+        if (command) command(message, args || '')
     }
 }
