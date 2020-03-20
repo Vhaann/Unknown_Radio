585d45c7d926e258ba829b31aaf69031b7dbcd8c
CamilleAbella <camille.abella@hotmail.fr>
2020-03-20 12:50:09 +0100

    update eval command

--- commands/js.js
+++ commands/js.js
@@ -2,11 +2,24 @@ const {
     RichEmbed
 } = require('discord.js')
 
+const admin = ['126292280700043264', '352176756922253321']
+const unauthorizedWords = ['process','eval','token','exit','require','module','export']
+const unauthorizedWordsWithRegex = {}
+for(const word of unauthorizedWords)
+    unauthorizedWordsWithRegex[word] = new RegExp(word.split('').join('[^a-z]*'),'im')
+
+
 module.exports = async function exe(message, js) {
 
-    if (!['126292280700043264', '352176756922253321'].includes(message.author.id)) return
+    if (!admin.includes(message.author.id))
+        for(const word in unauthorizedWordsWithRegex){
+            const regex = unauthorizedWordsWithRegex[word]
+            if(regex.test(message.content))
+                return message.channel.send(`L'utilisation du mot **${word}** est prohibée... (sauf pour les beaux gosses)`)
+        }
 
     js = js.replace('```js', '').replace('```', '').trim()
+    if(!js.includes('\n') && !js.includes('return')) js = 'return ' + js
 
     const embed = new RichEmbed()
     let result = 'té nul'
