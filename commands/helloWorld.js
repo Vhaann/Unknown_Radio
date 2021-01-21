ca67a48df1c85153f79e937e979bbf9b6a34cac7
koko <koriantin@hotmail.fr>
2021-01-21 17:26:47 +0100

    initial commito

--- commands/helloWorld.js
+++
@@ -1,8 +0,0 @@
-const { RichEmbed } = require('discord.js')
-
-module.exports = function ( message, args ) {
-    const embed = new RichEmbed()
-        .setTitle('Hello World !')
-        .setDescription( args )
-    message.channel.send( embed ).catch()
-}
