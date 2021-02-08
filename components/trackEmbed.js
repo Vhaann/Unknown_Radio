2786a4674208dad563436a448e9786d90d494e46
koko <koriantin@hotmail.fr>
2021-02-08 17:24:43 +0100

    Refactor trackEmbed

--- components/trackEmbed.js
+++ components/trackEmbed.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 
 module.exports = (track, message) => {
 	const trackEmbed = new Discord.MessageEmbed()
-		.setTitle(track.title)
-		.setThumbnail(track.thumbnail)
-		.setAuthor(track.author)
+		.setAuthor(track.title)
 		.setColor(0X1ED760)
-		.setDescription(track.description)
+		.setThumbnail(track.thumbnail)
+		.addField('Song Name', track.title, true)
+		.addField('Author', track.author, true)
 		.addField('Listen on Youtube', track.url)
 		.setFooter(track.requestedBy.username, message.author.displayAvatarURL())
 		.setTimestamp();
